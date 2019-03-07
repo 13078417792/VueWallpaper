50ae18bb1426d3fd3b37ea4b132bfb9ed05ef17d
zhouchijian <397201698@qq.com>
2019-03-07 15:45:26 +0800

    用API名称获取数据

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import api from './api'
 
 const instance = axios.create({
     baseURL: 'https://api.presstime.cn/',
@@ -20,5 +21,14 @@ wallpaper.interceptors.response.use(function ({data}) {
     // Do something with response error
     return Promise.reject(error.message || error.msg || error);
 });
+
+export function mobileFetch(api_name:string,params:object={}):Promise<object>{
+    if(!api.hasOwnProperty(api_name)){
+        throw new Error('API不存在')
+    }
+    const url = api[api_name]
+
+    return wallpaper.get(url,{params})
+}
 
 export default instance
