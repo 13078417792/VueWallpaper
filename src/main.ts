9e096bd27b550b4c1043e62e6cedb3614601c6b0
zhouchijian <397201698@qq.com>
2019-03-07 11:32:45 +0800

    入口文件 导入公共样式、vant UI库样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-
+import "@assets/styles/common.less"
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
 
 new Vue({
