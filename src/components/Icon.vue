eb7a41c559b50f7af8a6c5f35ebfe849d2ec2a19
zhouchijian <397201698@qq.com>
2019-03-07 11:31:09 +0800

    图标组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <i :class="this.className"></i>
+</template>
+
+<script>
+    import {Component,Vue} from 'vue-property-decorator'
+
+    @Component({
+        name: "Icon",
+        props:{
+            type:{
+                type:String,
+                required:true
+            }
+        },
+        computed:{
+            className(){
+                const {type} = this.$props
+                return ['iconfont',type?`icon-${type}`:'']
+            }
+        }
+    })
+    export default class Icon extends Vue {
+
+
+    }
+</script>
+
+<style scoped lang="less">
+    @import "//at.alicdn.com/t/font_1070235_ep2myctu77l.css";
+</style>
