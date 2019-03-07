277c525bfbcdd964729953a24af55798a088ea3c
zhouchijian <397201698@qq.com>
2019-03-07 11:45:44 +0800

    API URL

---
+++ src/utils/api.ts
@@ -0,0 +1,15 @@
+import url from './url'
+const {tencent_location_service} = url
+
+
+export default {
+    TECENT_LOCATION:tencent_location_service,
+
+    HOT:'vertical?order=hot',
+
+    NEW:'vertical?order=new',
+
+    CATEGORY:'category',
+
+    ALBUM:'album'
+}
