3c928ad2fa216c5ab193f0f15b65e9d350ff0f53
zhouchijian <397201698@qq.com>
2019-03-07 11:33:23 +0800

    配置

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    presets: [
+        '@babel/preset-env',
+        '@vue/app'
+    ],
+    plugins: [
+        ['import', {
+            libraryName: 'vant',
+            libraryDirectory: 'es',
+            style: true
+        }, 'vant']
+    ]
 }
