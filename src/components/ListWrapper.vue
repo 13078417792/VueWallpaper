9085345cf8f246d0d400966291e86654a8ab9fef
zhouchijian <397201698@qq.com>
2019-03-07 18:26:39 +0800

    列表展示包裹层

---
+++ src/components/ListWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="list-wrapper">
+        <div class="wrap--ef6cyr33 cl" :style="style">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ListWrapper",
+        props:{
+            space:{
+                type:[Number,String],
+                default:'-1em'
+            }
+        },
+        computed:{
+            style(){
+                return {
+                    marginRight:this.$props.space
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import "../assets/styles/var.less";
+    .list-wrapper{
+        width:100%;
+
+        .wrap--ef6cyr33{
+            margin-right:-@padding;
+        }
+    }
+</style>
