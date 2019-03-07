3bbfe5f5bdea5ccb8174849fc1ad7bb34e976e8a
zhouchijian <397201698@qq.com>
2019-03-07 18:27:06 +0800

    图片组件

---
+++ src/components/TyImage.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="image-container">
+        <div class="image" :style="style">
+
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "TyImage",
+        props:{
+            src:{
+                type:String,
+                required:true
+            },
+            radius:{
+                type:String,
+                default:'0px'
+            }
+        },
+        computed:{
+            style(){
+                const {$props:props} = this
+                return {
+                    backgroundImage:`url(${props.src})`,
+                    backgroundSize:'cover',
+                    borderRadius:props.radius
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import "../assets/styles/var.less";
+    .image-container{
+        display:inline-block;
+
+        .image{
+            background-color:@border-color;
+            width:100%;
+            height:100%;
+        }
+    }
+</style>
