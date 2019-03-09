06665c416a6684c3f305af20e523d3bdf4bfea04
zhouchijian <397201698@qq.com>
2019-03-09 16:17:45 +0800

    滚动组件

---
+++ src/components/Scroll.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="scroll-wrapper" ref="wrapper">
+        <div class="scroll-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Scroll",
+        data(){
+            return {
+                position:{
+                    x:0,y:0
+                }
+            }
+        },
+        activated(){
+            console.log('activated')
+        },
+        deactivated(){
+            console.log(1)
+        }
+    }
+</script>
+
+<style scoped lang="less">
+
+    .scroll-wrapper{
+        width:100%;
+        height:100%;
+        overflow-x:hidden;
+        overflow-y:auto;
+
+        .scroll-content{
+            width:100%;
+        }
+    }
+
+</style>
