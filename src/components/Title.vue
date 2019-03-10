82bd1e60439565d4b89442c176831bdd3a8986e0
zhouchijian <397201698@qq.com>
2019-03-10 15:31:27 +0800

    头部标题

---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3 class="title--header">
+        {{title}}
+    </h3>
+</template>
+
+<script>
+    export default {
+        name: "Title",
+        props:{
+            title:String
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .title--header{
+        color: #2c3e50;
+    }
+</style>
