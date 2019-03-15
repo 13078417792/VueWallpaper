28f7fadaeececa7f1dd0cc6f6125f7cac9283d79
zhouchijian <397201698@qq.com>
2019-03-15 22:29:30 +0800

    各API的请求函数和http通用请求方法挂载到Vue原型链上

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,15 @@ import './registerServiceWorker'
 import "@assets/styles/common.less"
 import 'vant/lib/index.css'
 import VueCookie from 'vue-cookie'
+import request from '@utils/request'
+import http from '@utils/http'
+
 Vue.use(VueCookie)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$request = request
+Vue.prototype.$http = http
 
 new Vue({
   router,
