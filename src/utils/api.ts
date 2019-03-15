c7410563b347977e1dc7037a9c39fe9353541521
zhouchijian <397201698@qq.com>
2019-03-15 22:03:01 +0800

    添加多个接口

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -11,5 +11,16 @@ export default {
 
     album:'/wallpaper/mobile/album',
 
-    login:'/wallpaper/std/v1/user/login'
+    login:'/wallpaper/std/v1/user/login',
+
+    logout:'/wallpaper/std/v2/user/logout',
+
+    // 获取竖屏壁纸评论
+    vertical_comment:'/wallpaper/std/v2/vertical/vertical/{$id}/comment',
+
+    // 获取横屏壁纸评论
+    horizontal_comment:'/wallpaper/std/v2/wallpaper/wallpaper/{$id}/comment',
+
+    // 发表壁纸评论
+    comment:'/wallpaper/std/v2/user/wallpaper/wallpaper/{$id}/comment'
 }
