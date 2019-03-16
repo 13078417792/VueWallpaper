3a624daf987fc7c33069eec51b82d40fdc7ce33d
zhouchijian <397201698@qq.com>
2019-03-16 17:41:22 +0800

    记录元素滚动位置

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -5,9 +5,11 @@
 <script>
     import CategoryItem from './Home/Category'
     import NavLayout from '@components/NavLayout'
+    import SaveScrollMixin from '@src/mixins/save-scroll'
 
     export default {
         name: "Category",
+        mixins:[SaveScrollMixin],
         components:{CategoryItem,NavLayout},
 
     }
