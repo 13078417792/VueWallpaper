66fbc224fc12a5f44d5395f585c19b39719819da
zhouchijian <397201698@qq.com>
2019-03-16 17:43:43 +0800

    个人中心

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-// import Category from './views/Category.vue'
 
 Vue.use(Router)
 export default new Router({
@@ -23,13 +22,15 @@ export default new Router({
             meta:{
                 nav:true
             }
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: ()=>import('./views/User.vue'),
+            meta:{
+                nav:true
+            }
         }
-    ],
-    // scrollBehavior(to, from, savedPosition) {
-    //     if(savedPosition){
-    //         return savedPosition
-    //     }
-    //     return {x:0,y:0}
-    // }
+    ]
 
 })
