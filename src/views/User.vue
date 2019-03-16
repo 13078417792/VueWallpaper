e18cb5164616fae444d296d7fdff3b0983892802
zhouchijian <397201698@qq.com>
2019-03-16 17:44:03 +0800

    个人中心页面组件

---
+++ src/views/User.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="user">
+
+        <Title title="个人中心" />
+
+        <template v-if="is_login">
+            <Tabs v-model="active" height="300px">
+                <TabItem title="收藏">
+                    <div class="">
+                        收藏233
+                    </div>
+                </TabItem>
+
+                <TabItem title="下载">
+                    <div class="">
+                        下载
+                    </div>
+                </TabItem>
+
+            </Tabs>
+        </template>
+
+        <Login v-else />
+
+
+    </div>
+</template>
+
+<script>
+    import Tabs from '@components/Tabs'
+    import TabItem from '@components/TabItem'
+    import Title from '@components/Title'
+    import {mapGetters,mapState} from 'vuex'
+    import Login from './UserLogin'
+
+    export default {
+        name: "User",
+        components:{Tabs,TabItem,Title,Login},
+        data(){
+            return {
+                active:'收藏'
+            }
+        },
+        computed:{
+            ...mapGetters({
+                is_login:'Auth/is_login',
+                auth:'Storage/auth'
+            }),
+            ...mapState({
+                cookies:'Storage/cookie'
+            }),
+        },
+        created(){
+
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    .user{
+        padding:0 1.5em;
+        overflow:auto;
+    }
+</style>
