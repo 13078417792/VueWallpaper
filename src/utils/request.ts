4a53652ea1fdab722c9480b6aa71b78da3d1d0ba
zhouchijian <397201698@qq.com>
2019-03-16 16:14:18 +0800

    响应类型

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,10 +2,16 @@ import http from './http'
 import api from './api'
 import VueCookie from 'vue-cookie'
 
+type requestResult = {
+    code:number,
+    msg:string,
+    sessionId:string|null|undefined
+}|any
+
 export const login = function(email:string,pwd:string,autoSave:boolean=true) :promise<any>{
     return http.post(api.login,{
         email,passwd:pwd
-    }).then(result=>{
+    }).then((result:requestResult)=>{
         const sessionId:string|null = result.sessionId
         if(!sessionId || result.code!==0){
             return Promise.reject(result.msg || '登录失败')
