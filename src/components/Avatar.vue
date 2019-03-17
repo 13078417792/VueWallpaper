a9e4b2d117dfd226ceeb05bca168f659065f7ceb
zhouchijian <397201698@qq.com>
2019-03-17 21:53:12 +0800

    头像组件

---
+++ src/components/Avatar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="ty-avatar" :style="style">
+
+        <img v-if="src" :src="src || ''" :style="style">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Avatar",
+        props:{
+            src:String,
+            size:String
+        },
+        computed:{
+            style(){
+                const props = this.$props
+                return {
+                    width:props.size,
+                    height:props.size
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="less">
+    @import "../assets/styles/var.less";
+
+    @default-size:45px;
+    .ty-avatar{
+        display:inline-block;
+        width:@default-size;
+        height:@default-size;
+        background-color:@border-color;
+        border-radius:50%;
+        overflow:hidden;
+
+        img{
+            display:block;
+            width:100%;
+            height:100%;
+        }
+    }
+</style>
