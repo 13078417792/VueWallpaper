042c058da5c9ec397340057e27fcc8c3d6f185d1
zhouchijian <397201698@qq.com>
2019-03-17 22:50:40 +0800

    专辑列表请求函数

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
 import http from './http'
 import api from './api'
 import VueCookie from 'vue-cookie'
+import {computed_skip as com_skip} from './helper'
 
 type requestResult = {
     code:number,
@@ -23,6 +24,43 @@ export const login = function(email:string,pwd:string,autoSave:boolean=true) :Pr
     })
 }
 
+
+type album_list_params_type = {
+    limit:number,
+    first:number,
+    order:"new"|"hot",
+    page:number
+}
+
+export const album_list = function(params:album_list_params_type={
+    limit:10,
+    first:1,
+    order:'new',
+    page:1
+}){
+    let {limit,first,order,page} = params
+    if(typeof first==='undefined'){
+        first = 1
+    }
+    if(typeof page==='undefined'){
+        page = 1
+    }
+    if(typeof order==='undefined'){
+        order = 'new'
+    }
+    const skip = com_skip(page,limit)
+    const data = {limit,first,order,skip}
+    return http.get(api.album,{
+        params:data
+    }).then(result=>{
+        return Promise.resolve(result)
+    })
+}
+
+export const album_detail = function(){
+
+}
+
 export default {
-    login
+    login,album_list
 }
