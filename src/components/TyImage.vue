d4ce63aa1252837c57e19a2ceb3981e9e0583bda
zhouchijian <397201698@qq.com>
2019-03-17 15:25:34 +0800

    图片转码base64

--- src/components/TyImage.vue
+++ src/components/TyImage.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="image-container">
+    <section class="image-container" >
         <div class="image" :style="style">
 
         </div>
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+    import {mapMutations,mapGetters} from 'vuex'
     export default {
         name: "TyImage",
         props:{
@@ -19,13 +20,46 @@
                 default:'0px'
             }
         },
+        data(){
+            return {
+                image_src:''
+            }
+        },
         computed:{
             style(){
-                const {$props:props} = this
+                const {$props:props,$data:data} = this
                 return {
                     backgroundImage:`url(${props.src})`,
                     backgroundSize:'cover',
                     borderRadius:props.radius
+                }
+            },
+            ...mapGetters({
+                getValue:'Storage/getValue'
+            })
+        },
+        created(){
+            // this.request_img()
+        },
+        methods:{
+            ...mapMutations({
+                updateImageBase:'Storage/updateImageBase'
+            }),
+            request_img(){
+                let src = this.getValue(this.$props.src)
+                if(!src){
+                    this.$helper.base64_img(this.$props.src,true).then(base=>{
+                        this.updateImageBase({
+                            src:this.$props.src,
+                            dataUrl:base
+                        })
+                        this.image_src = base
+                        console.log(base)
+                    }).catch(err=>{
+                        console.error(err)
+                    })
+                }else{
+                    this.image_src = src
                 }
             }
         }
