bc75e1382ac09ecb981e9c4faa079509ae87dbc9
zhouchijian <397201698@qq.com>
2019-03-17 22:42:02 +0800

    标题等级

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,14 +1,45 @@
 <template>
-    <h3 class="title--header">
+    <h1 class="title--header" v-if="title_level===1">
+        {{title}}
+    </h1>
+    <h2 class="title--header" v-else-if="title_level===2">
+        {{title}}
+    </h2>
+    <h3 class="title--header" v-else-if="title_level===3">
         {{title}}
     </h3>
+    <h4 class="title--header" v-else-if="title_level===4">
+        {{title}}
+    </h4>
+    <h5 class="title--header" v-else-if="title_level===5">
+        {{title}}
+    </h5>
+    <h6 class="title--header" v-else-if="title_level===6">
+        {{title}}
+    </h6>
 </template>
 
 <script>
     export default {
         name: "Title",
         props:{
-            title:String
+            title:String,
+            level:{
+                type:Number,
+                default:3
+            }
+        },
+        computed:{
+            title_level(){
+                const {level} = this.$props
+                if(level<=0){
+                    return 1
+                }
+                if(level>6){
+                    return 6
+                }
+                return level
+            }
         }
     }
 </script>
