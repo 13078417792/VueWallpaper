1ce9864e0211b635aa33e4f26881af8fdaad6726
zhouchijian <397201698@qq.com>
2019-03-19 17:47:25 +0800

    导入专辑详细组件

--- src/router.ts
+++ src/router.ts
@@ -6,31 +6,31 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-            meta:{
-                nav:true
-            }
-        },
-        {
-            path: '/category',
-            name: 'category',
-            component: ()=>import('./views/Category.vue'),
-            meta:{
-                nav:true
-            }
-        },
-        {
-            path: '/user',
-            name: 'user',
-            component: ()=>import('./views/User.vue'),
-            meta:{
-                nav:true
-            }
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: {
+            nav: true
         }
-    ]
+    }, {
+        path: '/category',
+        name: 'category',
+        component: () => import('./views/Category.vue'),
+        meta: {
+            nav: true
+        }
+    }, {
+        path: '/user',
+        name: 'user',
+        component: () => import('./views/User.vue'),
+        meta: {
+            nav: true
+        }
+    }, {
+        path: '/album/detail/:id',
+        name: 'album-detail',
+        component: () => import('./views/AlbumDetail.vue')
+    }]
 
 })
