11285a095ff2b903ba8fa4a100d9addffd437f2f
zhouchijian <397201698@qq.com>
2019-03-23 10:47:43 +0800

    如果cookie中存在未过期auth token，每次请求都带上

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,16 +2,23 @@ import axios from 'axios'
 import apis from './api'
 import {JsonEncoded} from './helper'
 
+const Cookies = require('js-cookie')
+
 export const api = apis
 
 const baseURL ='https://api.presstime.cn/'
-
+const picasso = 'picasso:'
+const sessionIdField = 'x-transmission-session-id'
 const instance = axios.create({
     baseURL,
     timeout: 1000,
     transformRequest:[function(data, headers){
         if(data){
             data = JsonEncoded(data)
+        }
+        const auth = Cookies.get('auth') || ''
+        if(auth){
+            headers[sessionIdField] = `${picasso}${auth}`
         }
         return data;
     }],
@@ -23,8 +30,8 @@ const instance = axios.create({
 instance.interceptors.response.use(function (response) {
     // Do something with response data
     let {data,headers} = response
-    const sessionIdField = 'x-transmission-session-id'
-    const picasso = 'picasso:'
+
+
 
     if(headers.hasOwnProperty(sessionIdField) && !!headers[sessionIdField] && headers[sessionIdField].indexOf(picasso)===0){
         data.sessionId = headers[sessionIdField].split(':')[1]
